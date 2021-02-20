0a3f22f44de82dfe76ae388abf3f91e3eceb7ba6
Michael Sana <77210893+MichaelSana@users.noreply.github.com>
2021-02-20 18:14:23 -0500

    Added Library and class contetns

---
+++ Library.java
@@ -0,0 +1,25 @@
+/**
+ * The singelton class to sort through the library
+ * @author Michael Sana
+ */
+import java.util.HashMap;
+public class Library{
+    HashMap<String,Integer> books = new HashMap<String,Integer>();
+    Library library;
+
+    private Library(){
+
+    }
+    public Library getInstance(){
+
+    }
+    public boolean checkoutBook(String bookName){
+
+    }
+    public void checkinBook(String bookName, int numToAdd){
+
+    }
+    public void displayBooks(){
+        
+    }
+}
