dd57fc0a1f9e7d9ac8f8187f11662a7681a722fe
Michael Sana <77210893+MichaelSana@users.noreply.github.com>
2021-02-20 20:52:04 -0500

    Finished code

--- Library.java
+++ Library.java
@@ -4,22 +4,45 @@
  */
 import java.util.HashMap;
 public class Library{
-    HashMap<String,Integer> books = new HashMap<String,Integer>();
+    HashMap<String,Integer> books = new HashMap<>();
     Library library;
 
-    private Library(){
+    /** This makes it so there can only be one instance of Library */
+    private Library(){}
 
-    }
     public Library getInstance(){
-
+        if(library == null){
+            System.out.println("Creating our library. Time to begin reading.");
+            library = new Library();
+        }
+        return library;
     }
     public boolean checkoutBook(String bookName){
-
+        if(books.containsKey(bookName) && books.get(bookName) >= 1 ){
+            books.replace(bookName, books.get(bookName) -1);
+            System.out.println(bookName + " was succesfully check out");
+            return true;
+        }
+        else{
+            System.out.println("Sorry " + bookName + " is not in stock");
+            return false;
+        }
     }
     public void checkinBook(String bookName, int numToAdd){
 
+        if(books.containsKey(bookName)){
+            books.replace(bookName, books.get(bookName) + numToAdd);
+            System.out.println("A new copy of " + bookName + " was added to the library");
+        }
+        else{
+            books.put(bookName, numToAdd);
+            System.out.println(bookName + " was added to the library");
+        }
     }
     public void displayBooks(){
-        
+        System.out.println("\nInventory:");
+        for(String value : books.keySet()){
+            System.out.println("\t " + value + ", copies " + books.get(value));
+        }
     }
 }
